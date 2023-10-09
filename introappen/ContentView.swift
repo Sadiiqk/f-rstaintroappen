501b5ce0370cf80e0e35096d59d090b87c6d91a4
sadiiq kaysa <sadiiqk@gmail.com>
2023-10-09 19:27:24 +0200

    Ändrard lite text

--- introappen/ContentView.swift
+++ introappen/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world mer text!")
+            Text("Hej världen!")
                 .font(.title)
                 .foregroundColor(Color("fancycolor"))
                 
