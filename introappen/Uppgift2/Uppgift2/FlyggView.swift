2d82fec25de63bb105384b5219d58710938ad7d9
sadiiq kaysa <sadiiqk@gmail.com>
2023-11-19 18:55:17 +0100

    Frist commit

--- introappen/Uppgift2/Uppgift2/FlyggView.swift
+++ introappen/Uppgift2/Uppgift2/FlyggView.swift
@@ -1,18 +1,62 @@
-//
-//  FlyggView.swift
-//  Uppgift2
-//
-//  Created by don falcone on 2023-11-19.
-//
-
 import SwiftUI
 
-struct FlyggView: View {
+struct PersonView: View {
+    @State private var stad = ["Malmö", "Skellefteå", "Karlstad"]
+    @State private var addName = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Image(systemName: "airplane")
+                    .imageScale(.large)
+                    .foregroundColor(.blue)
+                Text("Hej, resenärer!")
+
+                HStack {
+                    TextField("Skriv stad", text: $addName)
+                    Button("Lägg till annan stad") {
+                        addPerson()
+                    }
+                }
+
+                List(stad, id: \.self) { person in
+                    NavigationLink(destination: DetailView(plats: person)) {
+                        HStack {
+                            Image(systemName: "airplane")
+                                .foregroundColor(.blue)
+                            Text(person)
+                        }
+                    }
+                }
+
+                Button("Reset") {
+                    stad.removeAll()
+                }
+            }
+            .padding()
+        }
+    }
+
+    private func addPerson() {
+        if !addName.isEmpty {
+            stad.append(addName)
+            addName = ""
+        }
     }
 }
 
-#Preview {
-    FlyggView()
+struct DetailView: View {
+    var plats: String
+
+    var body: some View {
+        Text("Information om flygget till \(plats)")
+    }
 }
+
+struct PersonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonView()
+    }
+}
+
+
